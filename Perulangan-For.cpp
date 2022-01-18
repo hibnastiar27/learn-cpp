2545960ecec36a9622e8ae28f167c4cf0d5f7390
hibnastiar27 <2118078@scholar.itn.ac.id>
2022-01-18 22:59:30 +0700

    Perulangan-For

---
+++ Perulangan-For.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	// for
+	for (int a = 1; a <= 10; a++)
+	{
+		cout << a << endl;
+	}
+
+	cout << endl;
+	cout << endl;
+
+	//
+	for (int a = 1; a <= 10; a += 2)
+	{
+		cout << a << endl;
+	}
+
+	cout << endl;
+	cout << endl;
+
+	//
+	for (int a = 10; a >= 1; a -= 2)
+	{
+		cout << a << endl;
+	}
+}
